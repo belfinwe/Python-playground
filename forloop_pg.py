38938214a05c5f944a595f7685095721cedfb1a1
Joakim Nilsen <joakimrn02@gmail.com>
2020-02-08 06:45:19 +0100

    Added extraction of every other item with list list comprehension

--- forloop_pg.py
+++ forloop_pg.py
@@ -5,6 +5,17 @@ def double(in_data: int) -> int:
     return in_data * 2
 
 
-print(list(double(i) for i in list_i))
-# print(double(i) for i in list_i)
-print(list(map(double, list_i)))
+def alter_list(inn: list):
+    """
+    Returns a new list with onyl half the items
+    """
+    res = [inn[i] for i in range(len(inn)) if i%2 == 0]
+    return res
+
+
+
+if __name__ == "__main__":
+    print(list(double(i) for i in list_i))
+    # print(double(i) for i in list_i)
+    print(list(map(double, list_i)))
+    print(alter_list(list_i))
